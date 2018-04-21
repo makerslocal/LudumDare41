6cccd73488619db3e4794626543bffac2be01dc0
Jake Harris <milieu@javakat.io>
2018-04-21 17:05:29 -0500

    Add very basic scene framework

---
+++ Pins and Needles/Assets/Scripts/GameController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	private float raceTime;
+    
+	// Use this for initialization
+	void Start () {
+		raceTime = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		raceTime = Time.timeSinceLevelLoad - 3f;
+	}
+}

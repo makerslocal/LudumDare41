a449cb61363ea15732ddc6905c93a22e66bacab2
Jake Harris <milieu@javakat.io>
2018-04-21 17:25:49 -0500

    Swap to C# because autocomplete, also add timer to scene

--- Pins and Needles/Assets/Scripts/GameController.cs
+++ Pins and Needles/Assets/Scripts/GameController.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
 	private float raceTime;
+
+	public Text timerText;
     
 	// Use this for initialization
 	void Start () {
 		raceTime = 0f;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		raceTime = Time.timeSinceLevelLoad - 3f;
+		raceTime = Time.timeSinceLevelLoad;
+		timerText.text = "" + (int)(raceTime / 60f) + "\'" + (raceTime % 60) + "\"";
 	}
 }

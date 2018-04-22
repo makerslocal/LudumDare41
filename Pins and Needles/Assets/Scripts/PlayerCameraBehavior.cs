991ea89349bbfc36c4fa341d9f30d83f734cecda
Jake Harris <milieu@javakat.io>
2018-04-21 23:05:59 -0500

    Bind camera to car movement

---
+++ Pins and Needles/Assets/Scripts/PlayerCameraBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraBehavior : MonoBehaviour {
+
+	private Transform player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		player = GetComponent<Transform>().parent;
+		offset = GetComponent<Transform>().position - player.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		GetComponent<Transform>().position = player.position + offset;
+	}
+}

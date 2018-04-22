33e20977a29c7d488a712bc12add47c8d91cdfb9
Jake Harris <milieu@javakat.io>
2018-04-21 22:29:16 -0500

    Add fall detection to pins

---
+++ Pins and Needles/Assets/Scripts/PinBehavior.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinBehavior : MonoBehaviour {
+	public bool IsStanding () {
+		return (GetComponent<Transform>().rotation.x >= -79 || GetComponent<Transform>().rotation.x <= -101);
+	}
+}
